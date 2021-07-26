4995a170703d3254ae0056a5e90dd35386ec0047
JXPropertyCode <yellowdice92@hotmail.com>
2021-07-25 17:08:12 -0700

    added only drawing app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useRef, useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// hold reference to our canvas element
+	const canvasRef = useRef(null);
+	const contextRef = useRef(null);
+	const [isDrawing, setIsDrawing] = useState(false);
+
+	// initalize our canvas API when our component is mounted
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		canvas.width = window.innerWidth * 2;
+		canvas.height = window.innerHeight * 2;
+		canvas.style.width = `${window.innerWidth}px`;
+		canvas.style.height = `${window.innerHeight}px`;
+
+		// define 2d context to be able to draw
+		const context = canvas.getContext("2d");
+		context.scale(2, 2);
+		context.lineCap = "round"; // round endings for lines
+		context.strokeStyle = "black";
+		context.lineWidth = 5;
+		contextRef.current = context;
+	}, []);
+
+	// nativeEvent is derived from the
+	const startDrawing = ({ nativeEvent }) => {
+		const { offsetX, offsetY } = nativeEvent;
+		contextRef.current.beginPath();
+		contextRef.current.moveTo(offsetX, offsetY);
+		setIsDrawing(true);
+	};
+
+	const finishDrawing = () => {
+		contextRef.current.closePath();
+		setIsDrawing(false);
+	};
+	const draw = ({ nativeEvent }) => {
+		if (!isDrawing) {
+			return;
+		}
+		const { offsetX, offsetY } = nativeEvent;
+		contextRef.current.lineTo(offsetX, offsetY);
+		contextRef.current.stroke();
+	};
+
+	return (
+		<canvas
+			onMouseDown={startDrawing}
+			onMouseUp={finishDrawing}
+			onMouseMove={draw}
+			ref={canvasRef}
+		/>
+	);
 }
 
 export default App;
